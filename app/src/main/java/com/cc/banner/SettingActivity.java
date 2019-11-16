1bc660dc97193761105293fca9bde84e028802b6
chengcongcong <644898042@qq.com>
2019-11-17 03:29:58 +0800

    fix x

--- app/src/main/java/com/cc/banner/SettingActivity.java
+++ app/src/main/java/com/cc/banner/SettingActivity.java
@@ -1,13 +1,14 @@
 package com.cc.banner;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.cc.bannerlib.Banner;
 import com.cc.bannerlib.bean.BannerCorner;
